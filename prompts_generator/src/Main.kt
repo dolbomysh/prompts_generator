e8b7563cec1b3c4d7ffc7d7f16a3e475a020333d
Катюша Лукашевич <lukashevich.es@phystech.edu>
2025-03-24 23:49:15 +0300

    fixed issues

--- prompts_generator/src/Main.kt
+++ prompts_generator/src/Main.kt
@@ -2,33 +2,33 @@ import java.net.HttpURLConnection
 import java.net.URI
 import org.json.JSONArray
 import org.json.JSONObject
+import java.net.URL
 
-// Шаблонная строка
-class TemplatedString (val templatedString: String, val arguments: Map<String, Any>) {
-    override fun toString(): String {
-        var result = templatedString
-        arguments.forEach{
-            result = result.replace("[${it.key}]", it.value.toString())
-        }
+// Подстановка аргументов в шаблонную строку
 
-        if (result.findAnyOf(listOf("[", "]")) != null) {
-            throw IllegalArgumentException()
-        }
-
-        return result
+fun templatedStringInterpolation(templatedString: String, arguments: Map<String, Any>): String {
+    var result = templatedString
+    arguments.forEach{
+        result = result.replace("[${it.key}]", it.value.toString())
     }
+    if (result.findAnyOf(listOf("[", "]")) != null) {
+        throw IllegalArgumentException()
+    }
+    return result
 }
 
+private val url: URL = URI.create("https://api-inference.huggingface.co/models/google/flan-t5-large").toURL()
 // Отправка серии запросов
 fun sendPrompts(prompts: List<String>): Map<Int, String> {
-    val url = URI.create("https://api-inference.huggingface.co/models/google/flan-t5-large").toURL()
     val connection = url.openConnection() as HttpURLConnection
     connection.requestMethod = "POST"
     connection.setRequestProperty("Content-Type", "application/json")
     connection.setRequestProperty("Authorization", "Bearer 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")
     connection.doOutput = true
 
-    val jsonPrompts = JSONObject().apply { put("inputs", JSONArray(prompts)) }.toString().toByteArray()
+    val jsonPrompts = JSONObject().apply { put("inputs", JSONArray(prompts))
+                                           put("parameters", JSONObject().apply { put("max_length", 500) })
+                                         }.toString().toByteArray()
     connection.outputStream.use { os -> os.write(jsonPrompts) }
 
     val responseCode = connection.responseCode
@@ -43,44 +43,44 @@ fun sendPrompts(prompts: List<String>): Map<Int, String> {
 }
 
 // Генерация запросов
-fun generatePrompts(templatedPrompt: String, arguments: List<Map<String, Any>>): Map<Int, String> {
-    val prompts = arguments.map { TemplatedString(templatedPrompt, it).toString() }
+fun generatePrompts(seriesOfPrompts: SeriesOfPrompts): Map<Int, String> {
+    val prompts = seriesOfPrompts.arguments.map { templatedStringInterpolation(seriesOfPrompts.templatedPrompt, it) }
     return sendPrompts(prompts)
 }
 
+// Серия запросов после парсинга
+data class SeriesOfPrompts(val templatedPrompt: String, val arguments: List<Map<String, Any>>)
+
 // Обработка входных данных
-fun parseInput(): Pair<String,  List<Map<String, String>>> {
-    var templatedPrompt: String = String()
-    val arguments: MutableList<MutableMap<String, String>> = mutableListOf()
-    var line = readln()
-    while (line.trim() != "") {
-        templatedPrompt += line
-        line = readln()
-    }
-    val promptsCount: Int = readln().trim().toInt()
-    var keyValueList: List<String>
-    for (i in 0..< promptsCount) {
-        arguments += mutableMapOf()
-        for (keyValue in  readln().trim().split(", ", ",", " , ", " ,")) {
-            keyValueList = keyValue.split("=", " = ")
-            if (keyValueList.size != 2) {
-                throw IllegalArgumentException()
-            }
-            arguments[i] += Pair(keyValueList[0], keyValueList[1])
+fun parseInput(): SeriesOfPrompts {
+    val templatedPrompt: String = buildString {
+        var line = readln()
+        while (line.trim() != "") {
+            append(line)
+            line = readln()
         }
     }
-    return Pair(templatedPrompt, arguments)
+    val promptsCount: Int = readln().trim().toInt()
+    val arguments: List<MutableMap<String, String>> = List(promptsCount) { mutableMapOf() }
+    for (i in 0..< promptsCount) {
+        for (keyValue in readln().trim().split(", ", ",", " , ", " ,")) {
+            val keyValueList = keyValue.split("=", " = ")
+            require(keyValueList.size == 2)
+            arguments[i][keyValueList[0]] = keyValueList[1]
+        }
+    }
+    return SeriesOfPrompts(templatedPrompt, arguments)
 }
 
 // Красивый вывод
-fun printRespond(respond: Map<Int, String>): Unit {
+fun printResponse(respond: Map<Int, String>): Unit {
     for ((key, value) in respond) {
         println("$key : $value")
     }
 }
 
 fun main() {
-    val (templatedString, arguments)  = parseInput()
-    val respond = generatePrompts(templatedString, arguments)
-    printRespond(respond)
+    val seriesOfPrompts = parseInput()
+    val respond = generatePrompts(seriesOfPrompts)
+    printResponse(respond)
 }
